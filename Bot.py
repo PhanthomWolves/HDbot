10cfa2caf5f34b529f8746e530769e3dc5bfa16b
PhanthomWolves <37021484+PhanthomWolves@users.noreply.github.com>
2018-03-03 16:30:05 +0000

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -13,15 +13,6 @@ import json
 import struct
 from   PIL         import Image
 from   discord.ext import commands
-from   cogs import settings
-from   cogs import displayName
-from   cogs import readableTime
-from   cogs import getImage
-from   cogs import nullify
-from   cogs import progressBar
-from   cogs import userTime
-from   cogs import message
-from   cogs import dL
 try:
     from urllib.parse import urlparse
 except ImportError:
